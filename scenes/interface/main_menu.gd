8bafb68e146129709d0386c84e15374b8a005e48
Caleb Alexander <calebtalexander@gmail.com>
2024-03-09 01:22:34 -0500

    cleanup

---
+++ scenes/interface/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+@onready var button_load_level_debug = $VBoxContainer/LoadLevelDebug
+@onready var button_new_game = $VBoxContainer/NewGame
+@onready var button_load_level = $VBoxContainer/LoadGame
+@onready var button_options = $VBoxContainer/Options
+@onready var button_exit = $VBoxContainer/Exit
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
